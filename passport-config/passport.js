6e4de95953de66b2d99e0ff06a6bb97cecdbb1bf
simple-sifu <kattyntommy@gmail.com>
2020-06-30 23:11:23 -0400

    initialize passport with API keys.

--- passport-config/passport.js
+++ passport-config/passport.js
@@ -3,6 +3,25 @@ const passport = require('passport');
 
 const TwitterStrategy = require('passport-twitter');
 
+passport.use(new TwitterStrategy(
+    {
+        consumerKey: 'RDCmUQIkIJMCCIzChFRTkEIch',
+        consumerSecret: 'umolAhI2SJ9ZAPpzzEoBafJdzPQe4Bln9X2sy6I7AQlSxHH4wk',
+        callbackURL: 'http://localhost:3000/twitter/callback'
+    },
+    function(token, tokenSecret, profile, cb) {
+        return cb(null, profile);
+    })
+);
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+  });
+  
+passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+  });
+
 
 
 module.exports = passport;
